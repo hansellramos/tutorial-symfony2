dde20eefac61b2e31cf8eba017c8ee069515e5d6
hansellesteban <hansell.ramos@gmail.com>
2015-05-25 18:01:00 -0500

    Agregada copia de la base de datos

---
+++ database/database.sql
@@ -0,0 +1,276 @@
+-- MySQL dump 10.13  Distrib 5.6.21, for osx10.6 (x86_64)
+--
+-- Host: localhost    Database: mentornotas
+-- ------------------------------------------------------
+-- Server version	5.6.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Contrato`
+--
+
+DROP TABLE IF EXISTS `Contrato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Contrato` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `referencia` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tarifa_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_9F1F0775FE3B76B` (`tarifa_id`),
+  KEY `IDX_9F1F0775DB38439E` (`usuario_id`),
+  CONSTRAINT `FK_9F1F0775DB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario` (`id`),
+  CONSTRAINT `FK_9F1F0775FE3B76B` FOREIGN KEY (`tarifa_id`) REFERENCES `Tarifa` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Contrato`
+--
+
+LOCK TABLES `Contrato` WRITE;
+/*!40000 ALTER TABLE `Contrato` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Contrato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Etiqueta`
+--
+
+DROP TABLE IF EXISTS `Etiqueta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Etiqueta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `texto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_942AC46CDB38439E` (`usuario_id`),
+  CONSTRAINT `FK_942AC46CDB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Etiqueta`
+--
+
+LOCK TABLES `Etiqueta` WRITE;
+/*!40000 ALTER TABLE `Etiqueta` DISABLE KEYS */;
+INSERT INTO `Etiqueta` VALUES (1,'php',1),(2,'asp',1),(3,'javascript',1);
+/*!40000 ALTER TABLE `Etiqueta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Grupo`
+--
+
+DROP TABLE IF EXISTS `Grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Grupo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rol` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Grupo`
+--
+
+LOCK TABLES `Grupo` WRITE;
+/*!40000 ALTER TABLE `Grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Nota`
+--
+
+DROP TABLE IF EXISTS `Nota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Nota` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `texto` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `fecha` datetime NOT NULL,
+  `path` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_68E29133DB38439E` (`usuario_id`),
+  CONSTRAINT `FK_68E29133DB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Nota`
+--
+
+LOCK TABLES `Nota` WRITE;
+/*!40000 ALTER TABLE `Nota` DISABLE KEYS */;
+INSERT INTO `Nota` VALUES (1,'Nota de prueba 1','Texto para la nota de pruebas 1','2015-05-20 13:55:04','ruta/a/nota1',NULL),(2,'Nota de prueba 2','Texto para la nota de pruebas 2','2015-05-20 13:55:04','ruta/a/nota2',1);
+/*!40000 ALTER TABLE `Nota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Publicidad`
+--
+
+DROP TABLE IF EXISTS `Publicidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Publicidad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `texto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `path` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Publicidad`
+--
+
+LOCK TABLES `Publicidad` WRITE;
+/*!40000 ALTER TABLE `Publicidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Publicidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tarifa`
+--
+
+DROP TABLE IF EXISTS `Tarifa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tarifa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `periodo` int(11) NOT NULL,
+  `precio` double NOT NULL,
+  `validoDesde` date NOT NULL,
+  `validoHasta` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tarifa`
+--
+
+LOCK TABLES `Tarifa` WRITE;
+/*!40000 ALTER TABLE `Tarifa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Tarifa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `isActive` tinyint(1) NOT NULL,
+  `tokenRegistro` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuario`
+--
+
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+INSERT INTO `Usuario` VALUES (1,'hansel','ramos','jade','hansell.ramos','pass','hansell.ramos@gmail.com',1,'abcdefg');
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nota_etiqueta`
+--
+
+DROP TABLE IF EXISTS `nota_etiqueta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nota_etiqueta` (
+  `nota_id` int(11) NOT NULL,
+  `etiqueta_id` int(11) NOT NULL,
+  PRIMARY KEY (`nota_id`,`etiqueta_id`),
+  KEY `IDX_74ECDADEA98F9F02` (`nota_id`),
+  KEY `IDX_74ECDADED53DA3AB` (`etiqueta_id`),
+  CONSTRAINT `FK_74ECDADEA98F9F02` FOREIGN KEY (`nota_id`) REFERENCES `Nota` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_74ECDADED53DA3AB` FOREIGN KEY (`etiqueta_id`) REFERENCES `Etiqueta` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nota_etiqueta`
+--
+
+LOCK TABLES `nota_etiqueta` WRITE;
+/*!40000 ALTER TABLE `nota_etiqueta` DISABLE KEYS */;
+INSERT INTO `nota_etiqueta` VALUES (1,1),(1,2),(2,3);
+/*!40000 ALTER TABLE `nota_etiqueta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_grupo`
+--
+
+DROP TABLE IF EXISTS `usuario_grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario_grupo` (
+  `usuario_id` int(11) NOT NULL,
+  `grupo_id` int(11) NOT NULL,
+  PRIMARY KEY (`usuario_id`,`grupo_id`),
+  KEY `IDX_91D0F1CDDB38439E` (`usuario_id`),
+  KEY `IDX_91D0F1CD9C833003` (`grupo_id`),
+  CONSTRAINT `FK_91D0F1CD9C833003` FOREIGN KEY (`grupo_id`) REFERENCES `Grupo` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_91D0F1CDDB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_grupo`
+--
+
+LOCK TABLES `usuario_grupo` WRITE;
+/*!40000 ALTER TABLE `usuario_grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario_grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-25 18:00:21
